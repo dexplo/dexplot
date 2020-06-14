c4b77a57511a3eb37bf5d11fc70b29966f5b0cc4
tdpetrou <petrou.theodore@gmail.com>
2020-06-14 12:16:16 -0400

    refactor color viewer

--- dexplot/colors/__init__.py
+++ dexplot/colors/__init__.py
@@ -1,2 +1,5 @@
 from ._categories import sequential, diverging, cyclic, qualitative, misc, all_cmaps
-from ._app import ColorViewer
+
+import importlib
+if importlib.util.find_spec('ipywidgets') and importlib.util.find_spec('IPython'):
+    from ._app import color_viewer
