00eaab133f7b0dcfa82b60bf46755ddb0554ca63
tdpetrou <petrou.theodore@gmail.com>
2020-06-18 14:59:18 -0400

    import plotly

--- dexplot/__init__.py
+++ dexplot/__init__.py
@@ -1,5 +1,6 @@
 from ._plots import line, bar, box, scatter, violin, hist, count, kde
 from ._utils import load_dataset
+from ._plotly import bar_plotly, line_plotly, scatter_plotly, count_plotly, box_plotly, violin_plotly
 from . import colors
 
 __version__ = '0.1.4'
